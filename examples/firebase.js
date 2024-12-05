b05fed40d9fcb2c56b6845c442aec71345ca7952
Jakub Jankiewicz <jcubic@onet.pl>
2024-12-05 21:22:21 +0100

    add missing firebase config

---
+++ examples/firebase.js
@@ -0,0 +1,10 @@
+const firebase_config = {
+    apiKey: 'AIzaSyA5zqBzylFJKfyahh0AStlss5mosqk75jw',
+    authDomain: 'dialogue-bddd4.firebaseapp.com',
+    projectId: 'dialogue-bddd4',
+    storageBucket: 'dialogue-bddd4.appspot.com',
+    messagingSenderId: '39616195119',
+    appId: '1:39616195119:web:b872c371a915e3016574da'
+};
+
+export default firebase_config;
